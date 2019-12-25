4640d9a46ac5e88e07eb213fedafcff8257725a2
OleksandraTymchenko <OleksandraTymchenko@users.noreply.github.com>
2019-12-25 12:30:42 +0200

    added task01

---
+++ Lesson.java
@@ -0,0 +1,12 @@
+package com.aleksatym.task01;
+
+public class Lesson {
+
+    public static void main(String[] args) {
+        System.out.println("****  ****    *     *     *");
+        System.out.println("*     *  *   * *    * * * *");
+        System.out.println("**    ****  *   *   *  *  *");
+        System.out.println("*     *    * *** *  *     *");
+        System.out.println("****  *   *       * *     *");
+    }
+}
