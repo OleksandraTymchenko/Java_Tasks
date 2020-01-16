408088a1597161980cf576742b68f6f01512fa3b
OleksandraTymchenko <OleksandraTymchenko@users.noreply.github.com>
2020-01-16 18:14:58 +0200

    Update and rename Lesson_3Task03.java to Lesson3Task03.java

--- Lesson3Task03.java
+++ Lesson3Task03.java
@@ -1,6 +1,6 @@
 package com.alex.task03;
 
-public class Lesson_3Task03 {
+public class Lesson3Task03 {
     public static void main(String[] args) {
         final int WIDTH = 10;
         int line = 0;
