c2084b4628be3fc5c2ef9ce21c3207fb4b7bf90e
OleksandraTymchenko <OleksandraTymchenko@users.noreply.github.com>
2020-01-18 12:10:54 +0200

    Add files via upload

---
+++ Homework04Task02.java
@@ -0,0 +1,17 @@
+package com.alex.homework04;
+
+public class Homework04Task02 {
+    static void getByteCode(int x){
+        int y;
+        String str ="";
+        while (x != 0){
+            y = x%2;
+            str = y + str;
+            x = x/2;
+        }
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        getByteCode(1090900);
+    }
+}
