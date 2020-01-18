3e39fbcec7bae29009c5578f3b4d44a2b696b00a
OleksandraTymchenko <OleksandraTymchenko@users.noreply.github.com>
2020-01-18 12:13:57 +0200

    Update Homework04Task03.java

--- Homework04Task03.java
+++ Homework04Task03.java
@@ -1,6 +1,7 @@
 package com.alex.homework04;
 
 public class Homework04Task03 {
+    
     static void changeToString(int x){
         String str = "" + x;
         char firstChar = str.charAt(str.length()-1);
@@ -8,6 +9,7 @@ public class Homework04Task03 {
         String newString = Character.toString(firstChar) + Character.toString(lastChar);
         System.out.println( newString);
     }
+    
     public static void main(String[] args) {
         changeToString(689790);
     }
