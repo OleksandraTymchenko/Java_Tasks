61dd17d7a78d3f25f6073661064c7bdf93863ed9
OleksandraTymchenko <OleksandraTymchenko@users.noreply.github.com>
2020-01-28 21:22:41 +0200

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,82 @@
+package com.alextym.task06;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private String publisher;
+    private int year;
+    private int pages;
+    private int price;
+
+    public Book(int id, String name, String author, String publisher, int year, int pages, int price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        this.pages = pages;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void view() {
+        System.out.println("id = "+ id + ", name: " + name
+                + ", author: " + author + ", publisher: " + publisher
+                + ", year = " + year + ", pages = " + pages + ", price = " + price);
+    }
+}
